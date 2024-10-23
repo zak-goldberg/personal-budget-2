b9232fa29656c34f7ba84e54c86eb92ba09d0a01
Zak Goldberg <zak.goldberg@gmail.com>
2024-10-23 10:22:54 -0700

    Updated envelopeRepositories. All tests still passing.

--- repositories/envelopeRepositories.js
+++ repositories/envelopeRepositories.js
@@ -2,68 +2,75 @@ const { pgClient } = require('../configs/db');
 const { Envelope } = require('../models/class-definitions');
 
 const getEnvelopes = async () => {
-    // TO-DO: add into try/catch block
-    const res = await pgClient.query('SELECT * FROM envelopes;');
-    const envelopeArray = [];
-    for (row of res.rows) {
-        const envelopeItem = new Envelope (
-            row.id,
-            row.name,
-            row.description,
-            row.total_amount_usd
-        );
-        envelopeArray.push(envelopeItem);
+    try {
+        // Use pg to fetch all envelope records
+        const res = await pgClient.query('SELECT * FROM envelopes;');
+        // Create envelopeArray populate with instaces of the Envelope class for each envelope returned in the query
+        const envelopeArray = [];
+        for (row of res.rows) {
+            const envelopeItem = new Envelope (
+                row.id,
+                row.name,
+                row.description,
+                row.total_amount_usd
+            );
+            envelopeArray.push(envelopeItem);
+        }
+        // Return the populated array
+        return envelopeArray;
+    } catch (err) {
+        // Throw any errors that arise
+        throw err;
     }
-    return envelopeArray;
 };
 
 const getEnvelopeById = async (envelopeId) => {
-    let requestedEnvelope;
     try {
+        // Use pg to get the envelope record given the envelopeId from the database
         const res = await pgClient.query('SELECT id, name, description, total_amount_usd FROM envelopes WHERE id = $1;', [ envelopeId ]);
+        // Validate that a record was returned, if not throw a descriptive error
         if (res.rows.length === 0) throw new Error('ID not in DB.');
-        const resultObject = res.rows[0];
-        // console.log(`resultObject.total_amount_usd: ${typeof resultObject.total_amount_usd}`);
-        requestedEnvelope = new Envelope (
-            resultObject.id,
-            resultObject.name,
-            resultObject.description,
-            resultObject.total_amount_usd
+        // Create an instance of the Envelope class for the returned row
+        const requestedEnvelope = new Envelope (
+            res.rows[0].id,
+            res.rows[0].name,
+            res.rows[0].description,
+            res.rows[0].total_amount_usd
         );
+        // Return the instance of the envelope class
         return requestedEnvelope;
     } catch (err) {
-        // console.error(err.stack);
+        // Throw any errors that arise
         throw err;
     }
 };
 
 const createEnvelope = async (envelopeName, envelopeDescription, totalAmountUSD) => {
     try {
-        await pgClient.query('INSERT INTO envelopes (name, description, total_amount_usd) VALUES ($1, $2, $3);', 
+        // Use pg to create a new envelope given the inputs
+        // Query should return the new record so it can be returned by the function
+        const res = await pgClient.query('INSERT INTO envelopes (name, description, total_amount_usd) VALUES ($1, $2, $3) RETURNING id, name, description, total_amount_usd;', 
             [envelopeName, envelopeDescription, totalAmountUSD]
         );
-        // TO-DO: Remove this select query and using the RETURNING clause in the above query
-        const res = await pgClient.query('SELECT id, name, description, total_amount_usd FROM envelopes WHERE name = $1 AND description = $2 AND total_amount_usd = $3;',
-            [envelopeName, envelopeDescription, totalAmountUSD]
-        );
-        const resultObject = res.rows[0];
+        // Create an new instance of the envelope class for the new record
         const requestedEnvelope = new Envelope (
-            resultObject.id,
-            resultObject.name,
-            resultObject.description,
-            resultObject.total_amount_usd
+            res.rows[0].id,
+            res.rows[0].name,
+            res.rows[0].description,
+            res.rows[0].total_amount_usd
         );
+        // Return the instance of the envelope class
         return requestedEnvelope;
     } catch (err) {
-        // console.error(err.stack);
+        // Throw any errors that arise
         throw err;
     }
 };
 
 const updateEnvelope = async (envelopeId, envelopeName, envelopeDescription, totalAmountUSD) => {
     try {
-        console.log(`updateEnvelope(totalAmountUSD): ${totalAmountUSD}`);
-        // Query to update the database based on the given inputs
+        // Use pg to update the provided envelope record
+        // Query should return the new record so it can be returned by the function
         const updatedEnvelopeRes = await pgClient.query('UPDATE envelopes SET name = $1, description = $2, total_amount_usd = $3 WHERE id = $4 RETURNING id, name, description, total_amount_usd;', 
             [envelopeName, envelopeDescription, totalAmountUSD, envelopeId]
         );
@@ -76,32 +83,24 @@ const updateEnvelope = async (envelopeId, envelopeName, envelopeDescription, tot
             updatedEnvelopeRes.rows[0].description,
             updatedEnvelopeRes.rows[0].total_amount_usd
         );
+        // Return the instance of the envelope class
         return updatedEnvelope;
     } catch (err) {
+        // Throw any errors that arise
         throw err;
     }
 };
 
 const deleteEnvelope = async (envelopeId) => {
     try {
-        // Query to delete the record from the DB
+        // Use pg to delete the provided record from the DB
         await pgClient.query('DELETE FROM envelopes WHERE id = $1', [envelopeId]);
+        // Return the provided id if succesful
         return envelopeId;
     } catch (err) {
+        // Throw any errors that arise during the process
         throw err;
     }
 };
-
-/*
-getEnvelopeById('cats')
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err.stack));
-*/
-
-/*
-createEnvelope('Savings', 'For saving', '$5,000.00')
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err.stack));
-*/
 
 module.exports = { getEnvelopes, getEnvelopeById, createEnvelope, updateEnvelope, deleteEnvelope };
